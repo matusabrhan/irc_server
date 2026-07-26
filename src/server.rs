479245d4f33b0ecfa7e5be44776ecfc0c3db49bf
Matus Abrhan <matusabrhan27@gmail.com>
2026-07-26 22:46:29 +0200

    server context

--- src/server.rs
+++ src/server.rs
@@ -3,11 +3,17 @@ use crate::{manager::Manager, session::Session};
 use log::{debug, info};
 use std::collections::HashMap;
 use std::{net::SocketAddr, time::Duration};
+use tokio::net::TcpStream;
 use tokio::{net::TcpListener, sync::broadcast, task::JoinHandle, time::sleep};
 
 pub struct Server {
     handle: JoinHandle<()>,
     cancel: broadcast::Sender<()>,
+}
+
+struct ServerContext {
+    ids: Vec<usize>,
+    sessions: HashMap<usize, Session>,
 }
 
 impl Server {
@@ -20,47 +26,23 @@ impl Server {
                 .expect("could not start server");
             let (mut server_bus_local, server_bus_manager) = ServerBus::new_duplex();
             let manager = Manager::start(server_bus_manager);
-
-            //TODO: server context
-            let mut ids: Vec<usize> = (1..256).collect();
-            let mut sessions: HashMap<usize, Session> = HashMap::new();
+            let mut ctx = ServerContext::new();
 
             loop {
                 tokio::select! {
                     Ok((stream, addr)) = listener.accept() => {
-                        debug!("connection from {:}", addr);
-
-                        match ids.pop() {
-                            Some(id) => {
-                                let (session, request_sender) = Session::start(stream, id, manager.new_request_sender());
-                                sessions.insert(id, session);
-                                let _ = server_bus_local.send(ServerMessage::RegisterSession(id, request_sender));
-                            }
-                            None => {}
-                        }
+                        ctx.handle_new_session(stream, addr, &manager, &server_bus_local);
                     }
 
                     Some(msg) = server_bus_local.recv() => {
-                        match msg {
-                            ServerMessage::CloseSession(id) => {
-                                if let Some(session) = sessions.remove(&id) {
-                                    ids.push(id);
-                                    session.stop().await
-                                }
-                            }
-
-                            ServerMessage::RegisterSession(..) => {
-                                unreachable!();
-                            }
-
-                        }
+                        ctx.handle_server_msg(msg, &manager, &server_bus_local).await;
                     },
 
                     _ = cancel_rx.recv() => break,
                 }
             }
             manager.stop().await;
-            for session in sessions.values() {
+            for session in ctx.sessions.values() {
                 session.stop().await;
             }
         });
@@ -79,6 +61,55 @@ impl Server {
         }
         if !self.handle.is_finished() {
             self.handle.abort();
+        }
+    }
+}
+
+impl ServerContext {
+    fn new() -> Self {
+        Self {
+            ids: (1..256).collect(),
+            sessions: HashMap::new(),
+        }
+    }
+
+    fn handle_new_session(
+        &mut self,
+        stream: TcpStream,
+        address: SocketAddr,
+        manager: &Manager,
+        server_bus: &ServerBus,
+    ) {
+        match self.ids.pop() {
+            Some(id) => {
+                let (session, request_sender) =
+                    Session::start(stream, id, manager.new_request_sender());
+                self.sessions.insert(id, session);
+                let _ = server_bus.send(ServerMessage::RegisterSession(id, request_sender));
+                debug!("opened session from {:} with id {:}", address, id)
+            }
+            None => {}
+        }
+    }
+
+    async fn handle_server_msg(
+        &mut self,
+        msg: ServerMessage,
+        manager: &Manager,
+        server_bus: &ServerBus,
+    ) {
+        match msg {
+            ServerMessage::CloseSession(id) => {
+                if let Some(session) = self.sessions.remove(&id) {
+                    self.ids.push(id);
+                    session.stop().await;
+                    debug!("closed session with id {:}", id)
+                }
+            }
+
+            ServerMessage::RegisterSession(..) => {
+                unreachable!();
+            }
         }
     }
 }
